eae96fa1b81c20f0f55575680d5cd6cb6ed92bb4
Lupe Alvarado <guadalupe@ultra.me>
2023-04-02 08:12:33 -0700

    Express Generator

---
+++ models/partner.js
@@ -0,0 +1,6 @@
+{
+    "name": "partnerSchema",
+    "image": "images/mongo-logo.png",
+    "featured": false,
+    "description": "Need a new fishing pole, a tacklebox, or flies of all kinds? Stop by Mongo Fly Shop."
+}
---
+++ models/promotion.js
@@ -0,0 +1,7 @@
+{
+    "name": "Mountain Adventure",
+    "image": "images/breadcrumb-trail.jpg",
+    "featured": true,
+    "cost": 1299,
+    "description": "Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided."
+}
