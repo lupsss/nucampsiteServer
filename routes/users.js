7b587a2c58ae949e0ee59a2c1db0a6bb241f51fb
Lupe Alvarado <guadalupe@ultra.me>
2023-04-09 09:20:43 -0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+const outer = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
